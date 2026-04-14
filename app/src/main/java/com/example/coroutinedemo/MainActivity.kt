539aa7edee5430a641bbe8c5dac8abf50abd4e6d
artem1187 <bukinartem11@gmail.com>
2026-04-14 14:26:37 +0700

    Создание UI макета

--- app/src/main/java/com/example/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/example/coroutinedemo/MainActivity.kt
@@ -1,47 +1,62 @@
 package com.example.coroutinedemo
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.coroutinedemo.ui.theme.CoroutineDemoTheme
+import android.view.View
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.coroutinedemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var count: Int = 1
+
+    // Создаем CoroutineScope
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CoroutineDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupSeekBar()
+    }
+
+
+
+    private fun setupSeekBar() {
+        binding.seekBar.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                count = if (progress < 1) 1 else progress
+                binding.countText.text = "${count} coroutines"
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+            }
+        })
+    }
+
+    private suspend fun performTask(taskNumber: Int): Deferred<String> =
+        coroutineScope.async(Dispatchers.Main) {
+            delay(5_000) // Задержка 5 секунд
+            return@async "Finished Coroutine ${taskNumber}"
+        }
+
+    fun launchCoroutines(view: View) {
+        // Запускаем корутины в цикле от 1 до выбранного количества
+        for (i in 1..count) {
+            binding.statusText.text = "Started Coroutine ${i}"
+
+            coroutineScope.launch(Dispatchers.Main) {
+                val result = performTask(i).await()
+                binding.statusText.text = result
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CoroutineDemoTheme {
-        Greeting("Android")
     }
 }
